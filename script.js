7cb62f522096fb589108c03ea2624be8f1fdb005
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-04-05 17:01:57 +0200

    refactor: Move grid creation to its own function

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 "use strict";
 
-const GRID_SIZE = 600;
-
 (function () {
   function getCurrentYear() {
     return new Date().getFullYear();
   }
 
-  const gridContainer = document.querySelector(".grid");
-  gridContainer.style.width = `${GRID_SIZE}px`;
-  gridContainer.style.height = `${GRID_SIZE}px`;
+  function createGrid(squares) {
+    const gridContainer = document.querySelector(".grid");
+    let squareSize = `${gridContainer.offsetWidth / squares}px`;
 
-  let squaresPerSide = 16;
-  let squareSize = `${GRID_SIZE / squaresPerSide}px`;
+    for (let r = 0; r < squares; r++) {
+      const row = document.createElement("div");
+      row.classList.add("grid__row");
 
-  for (let r = 0; r < squaresPerSide; r++) {
-    const row = document.createElement("div");
-    row.classList.add("grid__row");
+      for (let c = 0; c < squares; c++) {
+        const square = document.createElement("div");
+        square.classList.add("grid__square");
+        square.style.width = squareSize;
+        square.style.height = squareSize;
+        row.appendChild(square);
+      }
 
-    for (let c = 0; c < squaresPerSide; c++) {
-      const square = document.createElement("div");
-      square.classList.add("grid__square");
-      square.style.width = squareSize;
-      square.style.height = squareSize;
-      row.appendChild(square);
+      gridContainer.appendChild(row);
     }
-
-    gridContainer.appendChild(row);
   }
 
   const copyright = document.querySelector(".copyright");
+
   copyright.textContent = `${copyright.textContent} ${getCurrentYear()}`;
+  createGrid(20);
 })();
