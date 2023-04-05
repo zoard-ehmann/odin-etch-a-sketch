fba37708c644f554675264bee0bbeb6e929dc7df
Zoard Ehmann <zoard.ehmann@gmail.com>
2023-04-05 14:13:41 +0200

    feat: Create a fix grid using flex and for loops

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const GRID_SIZE = 700;
+
+(function () {
+  const gridContainer = document.querySelector(".grid");
+  gridContainer.style.width = `${GRID_SIZE}px`;
+  gridContainer.style.height = `${GRID_SIZE}px`;
+
+  let squaresPerSide = 16;
+  let squareSize = `${GRID_SIZE / squaresPerSide}px`;
+
+  for (let r = 0; r < squaresPerSide; r++) {
+    const row = document.createElement("div");
+    row.classList.add("grid__row");
+
+    for (let c = 0; c < squaresPerSide; c++) {
+      const square = document.createElement("div");
+      square.classList.add("grid__square");
+      square.style.width = squareSize;
+      square.style.height = squareSize;
+      row.appendChild(square);
+    }
+
+    gridContainer.appendChild(row);
+  }
+})();
